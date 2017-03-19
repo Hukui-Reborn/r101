e385cd3981a1339b73504a997ed60df082edc814
Hukui <tckuikui1234@gmail.com>
2017-03-19 14:14:38 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -35,6 +35,28 @@ class GroupsController < ApplicationController
     end
   end
 
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功后"
+    else
+      flash[:waring] = "你已经是本讨论版的成员"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:notice] = "退出讨论版成功"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出？"
+    end
+    redirect_to group_path(@group)
+  end
+
   def destroy
     @group.destroy
     flash[:alert] = "Group deleted"
